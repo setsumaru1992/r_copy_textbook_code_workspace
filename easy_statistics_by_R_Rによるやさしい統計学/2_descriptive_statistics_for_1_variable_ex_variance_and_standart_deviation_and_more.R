d56f8c815f481bd09a9f8c5ffec5ced04bda6e06
setsumaru1992 <setsumaru@muriyari.work>
2021-08-22 16:15:47 +0900

    Rによるやさしい統計学 2章 微修正

--- easy_statistics_by_R_Rによるやさしい統計学/2_descriptive_statistics_for_1_variable_ex_variance_and_standart_deviation_and_more.R
+++ easy_statistics_by_R_Rによるやさしい統計学/2_descriptive_statistics_for_1_variable_ex_variance_and_standart_deviation_and_more.R
@@ -3,7 +3,7 @@ mean_test_a <- mean(test_a)
 
 # 分散(分散や標準偏差は散布度を測るために使用)
 ## 用意された関数を使わない愚直な書き方(標本分散: 分散計算時にデータ数で割る)
-deviation_from_mean_test_a <- test_a - mean_test_a # 平均からの偏差
+deviation_from_mean_test_a <- test_a - mean_test_a # 平均からの偏差(いわゆる「偏差」)
 square_of_deviation_from_mean_test_a <- deviation_from_mean_test_a ^ 2
 square_sum_of_deviation_from_mean_test_a <- sum(square_of_deviation_from_mean_test_a)
 
@@ -13,6 +13,11 @@ variance # 7.76
 ## 用意された関数を使用(不偏分散: 分散計算時にデータ数 - 1で割る)
 var(test_a) # 9.7
 
+
+
 # 標準偏差
-## 用意された関数を使わない愚直な書き方
+## 用意された関数を使わない愚直な書き方(標本分散を使用)
 standart_deviation <- sqrt(variance)
+
+## 用意された関数を使用(不偏分散を使用)
+sd(test_a)
